73ce532f716b3cf1aa7eebd65111ecaedb49389d
root <root@RBT-PC.localdomain>
2021-07-23 14:43:14 +0300

    added start script

--- index.js
+++ index.js
@@ -116,6 +116,8 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error', { statusCode, message, err });
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000..')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}..`)
 })
